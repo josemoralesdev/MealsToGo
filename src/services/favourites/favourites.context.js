1e0456d9e747350298205f2f7eb0ed96e2c3f8f3
Eduardo Morales <Eduardomorales.dev@gmail.com>
2021-05-04 20:45:26 -0700

    Added load & store functions for favs using AsyncStorage.

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,4 +1,5 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const FavouritesContext = createContext();
 
@@ -14,6 +15,34 @@ export const FavouritesContextProvider = ({ children }) => {
       return res.placeId !== restaurant.placeId;
     });
     setFavourites(newFavourites);
+  };
+
+  useEffect(() => {
+    loadFavourites();
+  }, []);
+
+  useEffect(() => {
+    saveFavourites(favourites);
+  }, [favourites]);
+
+  const saveFavourites = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem("@favourites", jsonValue);
+    } catch (e) {
+      console.log("Error storing:", e);
+    }
+  };
+
+  const loadFavourites = async () => {
+    try {
+      const value = await AsyncStorage.getItem("@favourites");
+      if (value !== null) {
+        setFavourites(JSON.parse(value));
+      }
+    } catch (e) {
+      console.log("Error loading:", e);
+    }
   };
 
   return (
