7100298bdb2cada084c89dc062375a0b21cd74ba
Eduardo Morales <Eduardomorales.dev@gmail.com>
2021-05-04 20:43:55 -0700

    Added favourites Bar component.

--- src/components/favourites/favourites-bar.component.js
+++ src/components/favourites/favourites-bar.component.js
@@ -1,5 +1,37 @@
-import React from 'react'
-import { Text } from 'react-native'
-export const FavouritesBar = () => {
-  return <Text>Favorites Bar</Text>;
-}
+import React from "react";
+import { ScrollView, TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+import { CompactRestaurantInfo } from "../restaurant/compact-restaurant-info.component";
+import { Spacer } from "../spacer/spacer.component";
+import { Text } from "../typography/text.component";
+
+const FavoritesWrapper = styled.View`
+  padding: 10px;
+`;
+
+export const FavouritesBar = ({ favourites, onNavigate }) => {
+  if (!favourites.length) {
+    return null;
+  }
+  return (
+    <FavoritesWrapper>
+      <Spacer size="medium" position="left">
+        <Text variant="caption">Favourites</Text>
+      </Spacer>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {favourites?.map((restaurant) => {
+          const key = restaurant.name;
+          return (
+            <Spacer key={key} position="left" size="medium">
+              <TouchableOpacity
+                onPress={() => onNavigate("RestaurantDetail", { restaurant })}
+              >
+                <CompactRestaurantInfo restaurant={restaurant} />
+              </TouchableOpacity>
+            </Spacer>
+          );
+        })}
+      </ScrollView>
+    </FavoritesWrapper>
+  );
+};
