a8f4fa676a9a4d2347c786ba32edca5726c0ac75
Eduardo Morales <Eduardomorales.dev@gmail.com>
2021-05-08 22:17:09 -0700

    Added ErrorContainer & AuthInput component styles.

--- src/features/account/components/account.styles.js
+++ src/features/account/components/account.styles.js
@@ -1,7 +1,7 @@
 //import React from "react";
 import styled from "styled-components/native";
 import { colors } from "../../../infrastructure/theme/colors";
-import { Button } from "react-native-paper";
+import { Button, TextInput } from "react-native-paper";
 
 export const AccountBackground = styled.ImageBackground.attrs({
   source: require("../../../../assets/home_bg.jpg"),
@@ -28,4 +28,24 @@ export const AuthButton = styled(Button).attrs({
   color: colors.brand.primary,
 })`
   padding: ${(props) => props.theme.space[2]};
+`;
+export const ErrorContainer = styled.View`
+  max-width: 300px;
+  align-items: center;
+  align-self: center;
+  margin-top: ${(props) => props.theme.space[3]};
+`;
+
+export const AuthInput = styled(TextInput).attrs({
+  theme: {
+    colors: {
+      primary: colors.brand.primary,
+    },
+  },
+})`
+  width: 300px;
+`;
+
+export const Title = styled.Text`
+  font-size: ${(props) => props.theme.fontSizes.title};
 `;
